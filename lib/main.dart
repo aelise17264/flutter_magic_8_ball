2684c865458075c1e3242e28cbd83c9f3b919419
Anna <aelise17264@gmail.com>
2024-11-10 18:26:21 -0700

    restructured app to be a stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.blue.shade800,
-            title: Align(
-              alignment: Alignment.center,
-              child: Text('Your Magic 8 Ball',
-                style: TextStyle(
-                  color: Colors.white,
+    HomePage()
+
+);
+
+class HomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+              home: Scaffold(
+                appBar: AppBar(
+                  backgroundColor: Colors.blue.shade800,
+                  title: Align(
+                    alignment: Alignment.center,
+                    child: Text('Your Magic 8 Ball',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                body: SafeArea(
+                    child: Container(
+                        color: Colors.blue,
+                        child: Center(
+                          child: Padding(
+                            padding: EdgeInsets.all(20),
+                            child: Image(image: AssetImage('images/ball1.png'))
+                          ),
+                        )
+                    )
                 ),
               ),
-            ),
-          ),
-          body: SafeArea(
-            child: Container(
-                color: Colors.blue,
-              child: Center(
-                child: Image(image: AssetImage('images/ball1.png')),
-              )
-          )
-        ),
-      ),
-    )
-);
+            );
+  }
+}
