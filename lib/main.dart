bb94b9239c98e612ed934d8fdedda358c42f2bac
Anna <aelise17264@gmail.com>
2024-11-10 18:50:23 -0700

    restructured app to give it a stateful widget so that the user interaction triggers a change in the UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
     HomePage()
@@ -22,17 +23,45 @@ class HomePage extends StatelessWidget{
                   ),
                 ),
                 body: SafeArea(
-                    child: Container(
-                        color: Colors.blue,
-                        child: Center(
-                          child: Padding(
-                            padding: EdgeInsets.all(20),
-                            child: Image(image: AssetImage('images/ball1.png'))
-                          ),
-                        )
-                    )
+                    child: magicBall()
                 ),
               ),
             );
+  }
+}
+
+class magicBall extends StatefulWidget{
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<magicBall>{
+
+  var answerNumber = 1;
+  var random = Random();
+
+
+  @override
+  Widget build(BuildContext context){
+    return Container(
+        color: Colors.blue,
+        child: Center(
+          child: Padding(
+              padding: EdgeInsets.all(20),
+              child: GestureDetector(
+                onTap: (){
+                  setState(() {
+                  answerNumber = random.nextInt(6);
+                  if(answerNumber == 0){
+                    answerNumber = 1;
+                  }
+                  print('tapped $answerNumber');
+                  });
+                },
+              child: Image(image: AssetImage('images/ball$answerNumber.png'))
+              )
+          ),
+        )
+    );
   }
 }
